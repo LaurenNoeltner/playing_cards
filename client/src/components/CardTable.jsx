951de0472676a9f5938e3d320746eba6fe7266bf
Lauren Noeltner <laurennoeltner@gmail.com>
2021-01-09 01:40:26 -0500

    reworked functions to fix asynchronicity problem, and it works, but every button needs to be clicked twice for some reason

--- client/src/components/CardTable.jsx
+++ client/src/components/CardTable.jsx
@@ -1,62 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "./Header";
 // import API from "./utils/CardDeck";
 import "../App.css";
 import cardBack from "./utils/cardBack.jpg";
 
 const CardTable = () => {
-    const [shuffledDeck, setShuffledDeck] = useState([]);
     const [deck, setDeck] = useState([]);
+    const [shuffledDeck, setShuffledDeck] = useState([]);
+    
     const [userCards, setUserCards] = useState([]);
     const [computerCards, setComputerCards] = useState([]);
-    const [userCardImg, setUserCardImg] = useState("");
+    // const [userCardImg, setUserCardImg] = useState("");
+    // const [computerCardImg, setComputerCardImg] = useState("");
+    // const [userPlayedCard, setUserPlayedCard] = useState({});
+    // const [computerPlayedCard, setComputerPlayedCard] = useState([]);
 
-    
+    // useEffect(() => {
+    //     getDeck();
+    // }, []);
 
 
-    
     //establish connection to my JSON file in /public folder
-    const getDeck = (e) => {
-        e.preventDefault();
+    const getDeck = () => {
 
         fetch("./data.json").then(response => {
             console.log(response);
             return response.json();
         }).then(data => {
+            console.log(data, "first deck - unshuffled");
+            // localStorage.setItem(data, JSON.stringify(data));
+            // setDeck(JSON.parse(localStorage.getItem(data)));
             setDeck(data);
-            setUserCardImg(data[0].image)
-            // console.log(data);
+            // console.log(deck, "deck log");
             
+        }).then(() => {
+            console.log(deck, "deck log");
         }).catch(err => {
-            console.log(err + "couldn't get deck")
+            console.log(err, "couldn't get deck");
         });
-        shuffleDeck(deck);
-        setShuffledDeck(deck);
-        console.log(shuffledDeck);
-        // dealWar();
-        // API.makeDeck().then((res) => {
-        //     console.log(res.data);
-        //     setShuffledDeck(res.data.cards);
-        // }); 
     };
 
-    const shuffleDeck = (deck) => {
+    const randomize = (pile) => {
         
-        let currentIndex = deck.length, tempValue, randomIndex;
-
+        let currentIndex = pile.length, tempValue, randomIndex;
+        
         while (0 !== currentIndex) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
 
-            tempValue = deck[currentIndex];
-            deck[currentIndex] = deck[randomIndex];
-            deck[randomIndex] = tempValue;
+            tempValue = pile[currentIndex];
+            pile[currentIndex] = pile[randomIndex];
+            pile[randomIndex] = tempValue;
         }
-        return deck;
+        return pile;
+        
+    };
+    
+
+    const shuffleDeck = () => {
+        randomize(deck);
+        setShuffledDeck(deck);
+        console.log(shuffledDeck);
     };
 
+
+    
+   
+
+    
+
     const dealWar = () => {
-        //just setting the halfway point of the deck
+        //setting the halfway point of the deck
         let halfwayThruDeck = Math.floor(shuffledDeck.length / 2);
         //doing the real job
         let firstHalfDeck = shuffledDeck.slice(0, halfwayThruDeck);
@@ -67,13 +81,12 @@ const CardTable = () => {
         console.log(computerCards);
     };
 
-    //Initiates game
-    const startGameWar = (e) => {
-        getDeck(e);
-        dealWar();
-    };
-
-
+    // const flipCard = () => {
+    //     setUserPlayedCard(userCards[0]);
+    //     console.log(userCards[0]);
+    //     setComputerPlayedCard(computerCards[0]);
+    //     console.log(computerPlayedCard);
+    // }
 
 
 
@@ -82,7 +95,11 @@ const CardTable = () => {
             <Header />
             <div className="row">
                 <div className="col-12">
-                    <button className="startBtn" onClick={startGameWar}>Start Game</button>
+                    <button className="startBtn" onClick={getDeck}>Get Deck</button>
+                    <button className="startBtn" onClick={shuffleDeck}>Shuffle</button>
+                    <button className="startBtn" onClick={dealWar}>Deal Cards</button>
+                    {/* <button className="startBtn" onClick={startGameWar}>Start Game</button>
+                    <button className="startBtn" onClick={startGameWar}>Start Game</button> */}
                     {/* <button className="takeHandBtn" onClick={drawHands}>Get Hand</button> */}
 
                 </div>
@@ -91,22 +108,22 @@ const CardTable = () => {
 
             <div className="row opponentRow">
                 <div className="col-6 opponentDeck">
-                    <img className="cardImage" src={cardBack} />
+                    <img alt="card_piles" className="cardImage" src={cardBack} />
                 </div>
                 <div className="col-6 opponentDrawnCard">
-                    <img src="#" alt="computer_drawn_card" />
+                    <img className="cardImage" alt="computer_drawn_card" src="#" />
                 </div>
             </div>
             
             <div className="row userRow">
                 
                 <div className="col-6 userDeck">
-                    <img className="cardImage" src={cardBack} />
+                    <img alt="card_piles" className="cardImage" src={cardBack} />
 
                 </div>
                 <div className="col-6 userDrawnCard">
                     {/* <img src={userCards[0].image} alt="user_drawn_card" /> */}
-                    <img className="cardImage" src={userCardImg} />                    
+                    <img className="cardImage" alt="user_drawn_card" src="#" />                    
 
                 </div>
 
